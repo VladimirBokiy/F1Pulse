ff4b3f48035bba656e83148e0afde90582f448f7
VladimirBokiy <vl.bokiy@gmail.com>
2023-05-11 14:24:51 +0300

    update parser; update front

--- F1Pulse/Pages/News/Menu.cshtml.cs
+++ F1Pulse/Pages/News/Menu.cshtml.cs
@@ -10,37 +10,32 @@ public class NewsModel : PageModel
 
     private readonly HtmlParser _htmlParser;
     public List<Services.Parser.News> NewsList { get; set; } = new List<Services.Parser.News>();
-
     [BindProperty(SupportsGet = true)]
-    public int Year { get; set; } = DateTime.Today.Year;
-    [BindProperty(SupportsGet = true)]
-    public int Month { get; set; } = DateTime.Today.Month;
-    [BindProperty(SupportsGet = true)]
-    public int Day { get; set; } = DateTime.Today.Day;
+    public InputModel Input { get; set; } = new InputModel();
 
     public NewsModel()
     {
         _htmlParser = new HtmlParser();
-        
-        if (Year == 0 && Month == 0 && Day == 0)
-        {
-            Year = DateTime.Today.Year;
-            Month = DateTime.Today.Month;
-            Day = DateTime.Today.Day;
-        }
-
-        
     }
     public void OnGet()
     {
-        ViewData["day"] = Day;
-        ViewData["month"] = Month;
-        ViewData["year"] = Year;
-        
-        _htmlParser.Day = Day.ToString();
-        _htmlParser.Year = Year.ToString();
-        _htmlParser.Month = Month.ToString();
-        NewsList = _htmlParser.GetNews();
-        Console.WriteLine(NewsList.Count + " items");
+        _htmlParser.Day = Input.Day.ToString();
+        _htmlParser.Year = Input.Year.ToString();
+        _htmlParser.Month = Input.Month.ToString();
+        NewsList = _htmlParser.GetNewsList();
+    }
+
+    public IActionResult OnPost()
+    {
+        Console.WriteLine(Input.Day + " " + Input.Month + " " + Input.Year);
+
+        return RedirectToPage("/News/Menu", new { year = Input.Year, month = Input.Month, day = Input.Day });
+    }
+
+    public class InputModel
+    {
+        public int Year { get; set; } = DateTime.Today.Year;
+        public int Month { get; set; } = DateTime.Today.Month;
+        public int Day { get; set; } = DateTime.Today.Day;
     }
 }
--- F1Pulse/Services/Parser/HtmlParser.cs
+++ F1Pulse/Services/Parser/HtmlParser.cs
@@ -10,7 +10,7 @@ public class HtmlParser
 
     
 
-    public List<News> GetNews()
+    public List<News> GetNewsList()
     {
         List<News> newsList = new List<News>();
         
@@ -37,6 +37,10 @@ public class HtmlParser
                 news.FullContentLink = "https://www.f1news.ru" + news.FullContentLink;
             }
             if (news.FullContentLink.EndsWith(".shtml") || !news.FullContentLink.StartsWith("https://www.f1news.ru/news/f1-"))
+            {
+                continue;
+            }
+            if (newsList.Count != 0 && news.FullContentLink.Equals(newsList.Last().FullContentLink))
             {
                 continue;
             }
@@ -88,5 +92,47 @@ public class HtmlParser
             newsList.Add(news);
         }
         return newsList;
+    }
+
+    public News GetNews(string id)
+    {
+        News news = new();
+        
+        HtmlWeb fullHtmlWeb = new HtmlWeb();
+        HtmlDocument fullNews = fullHtmlWeb.Load("https://www.f1news.ru/news/f1-" + id + ".html");
+        Console.WriteLine("https://www.f1news.ru/news/f1-" + id + ".html");
+        
+        news.PostDate = fullNews.DocumentNode.Descendants("div")
+            .First(n => n.Attributes["class"] != null &&
+                        n.Attributes["class"].Value.Equals("post_date"))
+            .InnerText;
+        //Console.WriteLine(news.PostDate);
+
+        news.Title = fullNews.DocumentNode.Descendants("div")
+            .First(n => n.Attributes["class"] != null &&
+                        n.Attributes["class"].Value.Equals("post_head"))
+            .Element("h1")
+            .InnerText;
+        //Console.WriteLine(news.Title);
+
+        foreach (var p in fullNews.DocumentNode.Descendants("div")
+                     .First(n => n.Attributes["class"] != null &&
+                                 n.Attributes["class"].Value.Equals("post_content"))
+                     .Elements("p"))
+        {
+            news.Content.Add(p.InnerText);
+        }
+        
+        news.ImageLink = fullNews.DocumentNode.Descendants("div")
+            .First(n => n.Attributes["class"] != null &&
+                             n.Attributes["class"].Value.Equals("post_thumbnail"))
+            .Element("img")
+            .GetAttributeValue("src", "default");
+        if (news.ImageLink.StartsWith("//"))
+        {
+            news.ImageLink = "https:" + news.ImageLink;
+        }
+
+        return news;
     }
 }
