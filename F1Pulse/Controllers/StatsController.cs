6d954e32eec8e833f6f5ef1bf16e667d907e640e
VladimirBokiy <vl.bokiy@gmail.com>
2023-11-02 22:59:31 +0300

    stats controller added; stats views templates added; consStandings done;

---
+++ F1Pulse/Controllers/StatsController.cs
@@ -0,0 +1,65 @@
+﻿using F1Pulse.Models.Stats;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F1Pulse.Controllers;
+
+[Route("Stats/")]
+public class StatsController : Controller
+{
+    [HttpGet]
+    [Route("")]
+    [Route("Menu")]
+    public IActionResult Menu()
+    {
+        MenuModel model = new MenuModel();
+        return View(model);
+    }
+    
+    [HttpGet]
+    [Route("ConstructorStandings")]
+    public IActionResult ConstructorStandings()
+    {
+        ConstructorStandingsModel model = new ConstructorStandingsModel();
+        return View(model);
+    }
+    
+    [HttpGet]
+    [Route("DriverStandings")]
+    public IActionResult DriverStandings()
+    {
+        DriverStandingsModel model = new DriverStandingsModel();
+        return View(model);
+    }
+    
+    [HttpGet]
+    [Route("LapPositions")]
+    public IActionResult LapPositions()
+    {
+        LapPositionsModel model = new LapPositionsModel();
+        return View(model);
+    }
+
+    [HttpGet]
+    [Route("LapTimes")]
+    public IActionResult LapTimes()
+    {
+        LapTimesModel model = new LapTimesModel();
+        return View(model);
+    }
+    
+    [HttpGet]
+    [Route("QualifyingResults")]
+    public IActionResult QualifyingResults()
+    {
+        QualifyingResultsModel model = new QualifyingResultsModel();
+        return View(model);
+    }
+    
+    [HttpGet]
+    [Route("RaceResults")]
+    public IActionResult RaceResults()
+    {
+        RaceResultsModel model = new RaceResultsModel();
+        return View(model);
+    }
+}
--- F1Pulse/Models/Stats/ConstructorStandingsModel.cs
+++ F1Pulse/Models/Stats/ConstructorStandingsModel.cs
@@ -6,4 +6,15 @@ public class ConstructorStandingsModel
 {
     public int Year { get; set; }
     public List<SelectListItem> YearList { get; set; }
+
+    public ConstructorStandingsModel()
+    {
+        List<SelectListItem> yearList = new List<SelectListItem>();
+        for (int i = DateTime.Today.Year; i >= 1996; i--)
+        {
+            yearList.Add(new SelectListItem{Value = i.ToString(), Text = i.ToString()});
+        }
+
+        YearList = yearList;
+    }
 }
