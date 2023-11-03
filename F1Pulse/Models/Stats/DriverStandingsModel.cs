2ec3a77f92e61012765afdd5529a214ecc503cd2
VladimirBokiy <vl.bokiy@gmail.com>
2023-11-03 14:20:16 +0300

    Driver standings representation done

--- F1Pulse/Models/Stats/DriverStandingsModel.cs
+++ F1Pulse/Models/Stats/DriverStandingsModel.cs
@@ -1,6 +1,21 @@
-﻿namespace F1Pulse.Models.Stats;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace F1Pulse.Models.Stats;
 
 public class DriverStandingsModel
 {
+    public int Year { get; set; }
+
+    public List<SelectListItem> YearList { get; set; }
     
+    public DriverStandingsModel()
+    {
+        List<SelectListItem> yearList = new List<SelectListItem>();
+        for (int i = 2023; i >= 1996; i--)
+        {
+            yearList.Add(new SelectListItem{Value = i.ToString(), Text = i.ToString()});
+        }
+
+        YearList = yearList;
+    }
 }
