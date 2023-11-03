48bcf47d75f9c57cb0563d702b0b25506db54c75
VladimirBokiy <vl.bokiy@gmail.com>
2023-11-03 16:34:26 +0300

    qualifying results representation is done

--- F1Pulse/Models/Stats/QualifyingResultsModel.cs
+++ F1Pulse/Models/Stats/QualifyingResultsModel.cs
@@ -1,6 +1,20 @@
-﻿namespace F1Pulse.Models.Stats;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace F1Pulse.Models.Stats;
 
 public class QualifyingResultsModel
 {
+    public List<SelectListItem> YearList { get; set; }
+    public int Year { get; set; }
     
+    public QualifyingResultsModel()
+    {
+        List<SelectListItem> yearList = new List<SelectListItem>();
+        for (int i = 2023; i >= 1996; i--)
+        {
+            yearList.Add(new SelectListItem{Value = i.ToString(), Text = i.ToString()});
+        }
+
+        YearList = yearList;
+    }
 }
