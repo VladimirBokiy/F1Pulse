be8e9a1b43fb7f8b3ea6c6868d664c5312a743b6
VladimirBokiy <vl.bokiy@gmail.com>
2023-11-03 16:37:23 +0300

    race results representation is done

--- F1Pulse/Models/Stats/RaceResultsModel.cs
+++ F1Pulse/Models/Stats/RaceResultsModel.cs
@@ -1,6 +1,21 @@
-﻿namespace F1Pulse.Models.Stats;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace F1Pulse.Models.Stats;
 
 public class RaceResultsModel
 {
+    public List<SelectListItem> YearList { get; set; }
     
+    public string Year { get; set; }
+    
+    public RaceResultsModel()
+    {
+        List<SelectListItem> yearList = new List<SelectListItem>();
+        for (int i = 2023; i >= 1996; i--)
+        {
+            yearList.Add(new SelectListItem{Value = i.ToString(), Text = i.ToString()});
+        }
+
+        YearList = yearList;
+    }
 }
